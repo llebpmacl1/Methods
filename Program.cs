7e8e0e8069cc113d3339fea64b9464e149b37c29
LC <llebpmacl1@gmail.com>
2021-02-05 17:01:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Friend. What's your name?");
+            string input = Console.ReadLine();
+            string Hello = HelloMethod(input);
+            Console.WriteLine(Hello);
+        }
+        private static string HelloMethod(string name)
+        {
+            string sentence = "Hello " + name + "!";
+
+            return sentence;
+        }
+    }
+}
